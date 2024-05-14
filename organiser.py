007b27bbcf4ac3f8ca57b080cb203e5f0d8ac402
Nicole <98280662+nicoledsk@users.noreply.github.com>
2024-05-14 11:53:24 +0100

    Added docstrings, and formatted with Black

--- organiser.py
+++ organiser.py
@@ -1,27 +1,44 @@
 import os
 from pathlib import Path
-import argparse 
-parser = argparse.ArgumemtParser(description='file organiser')
-parser.add_argument('--sort', type=bool, store=True, help='enter "s" to sort files')
+import argparse
+
+parser = argparse.ArgumentParser(description="file organiser")
+parser.add_argument("--sort", action="store_true", help='enter "s" to sort files')
 args = parser.parse_args()
-   
+
 SUBDIRECTORIES = {
-        "DOCUMENTS": ['.pdf', '.rtf', '.txt'],
-        "AUDIO": ['.m4a', '.m4b', '.mp3'],
-        "VIDEOS": ['.mov', '.avi', '.mp4'],
-        "IMAGES": ['.jpg', '.jpeg', '.png']
-    }
+    "DOCUMENTS": [".pdf", ".rtf", ".txt"],
+    "AUDIO": [".m4a", ".m4b", ".mp3"],
+    "VIDEOS": [".mov", ".MOV" ".avi", ".mp4"],
+    "IMAGES": [".jpg", ".jpeg", ".png"],
+}
+
 if args.sort:
 
-    def pickDirectory(value):
+    def pickDirectory(value: str):
+        """This function loops with the dictionary's items and returns the category in which the suffix exists.
+        Args:
+        Values (iterable): The values of the items in the dictionary represnted as a str.
+
+        Returns:
+        Folder based on file extension.
+
+        """
         for category, suffixes in SUBDIRECTORIES.items():
             for suffix in suffixes:
                 if suffix == value:
                     return category
-                return 'MISC'
-    print(pickDirectory('.pdf'))
+                return "MISC"
+
+    # Organising directory. For loop that loops through all the files. usin 'Path' to get file paths
     def organiseDirectory():
+        """Loops through every item in the current working directory to get the file type, so we can properly organize it. Path is also pulled for each item so that we can move them.
+        Calls function.
+
+        """
         for item in os.scandir():
+            if item.is_dir():
+                continue
             filePath = Path(item)
             filetype = filePath.suffix.lower()
             directory = pickDirectory(filetype)
@@ -29,3 +46,5 @@ if args.sort:
             if directoryPath.is_dir() != True:
                 directoryPath.mkdir()
             filePath.rename(directoryPath.joinpath(filePath))
+
+    organiseDirectory()
